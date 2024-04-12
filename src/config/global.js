989f42173037648fbd42f15998ac7c25de551958
veimarcm <vcelis@sena.edu.co>
2024-04-11 23:48:06 -0500

    DU

--- src/config/global.js
+++ src/config/global.js
@@ -192,7 +192,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/122112_CF02_DU',
       },
       {
         icono: 'fas fa-download',
